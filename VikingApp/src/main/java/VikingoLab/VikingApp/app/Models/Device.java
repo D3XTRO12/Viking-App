5db07283d5570b6b122dfb68a7f26face6643991
D3XTRO12 <mirazopablo@gmail.com>
2024-05-11 19:43:51 -0300

    #11 Device model, with specific data of the device, and a relation w/ client table

---
+++ VikingApp/src/main/java/VikingoLab/VikingApp/app/Models/Device.java
@@ -0,0 +1,39 @@
+package VikingoLab.VikingApp.app.Models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "device")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Device {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
+    private Long id;
+
+    @Column(name = "device_type")
+    private String type;
+
+    @Column(name = "device_brand")
+    private String brand;
+
+    @Column(name = "device_model")
+    private String model;
+
+    @Column(name = "serial_number")
+    private String serialNumber;
+    
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private Client client;
+
+    public Device(Long id) {
+        this.id = id;
+    }
+}
