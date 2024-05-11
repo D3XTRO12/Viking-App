417e2058739b71706e07ff9e26f3cc1e8bb4dca1
D3XTRO12 <mirazopablo@gmail.com>
2024-05-11 19:53:06 -0300

    #8 client repo working for POST & GET

--- VikingApp/src/main/java/VikingoLab/VikingApp/app/Repositories/ClientRepo.java
+++ VikingApp/src/main/java/VikingoLab/VikingApp/app/Repositories/ClientRepo.java
@@ -3,11 +3,11 @@ package VikingoLab.VikingApp.app.Repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import VikingoLab.VikingApp.app.Models.ClientModel;
+import VikingoLab.VikingApp.app.Models.Client;
 
 @Repository
-public interface ClientRepo extends JpaRepository<ClientModel, Long>{
+public interface ClientRepo extends JpaRepository<Client, Long> {
 
-    
-    
+    Client findByDni(int dni);
+
 }
