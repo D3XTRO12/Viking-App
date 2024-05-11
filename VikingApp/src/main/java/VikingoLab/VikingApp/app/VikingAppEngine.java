30e79686840d18d6b2f9ac762c0d205ad2bd95be
D3XTRO12 <mirazopablo@gmail.com>
2024-05-11 19:37:11 -0300

    #10 Main File Name Changed

---
+++ VikingApp/src/main/java/VikingoLab/VikingApp/app/VikingAppEngine.java
@@ -0,0 +1,13 @@
+package VikingoLab.VikingApp.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VikingAppEngine {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VikingAppEngine.class, args);
+	}
+
+}
