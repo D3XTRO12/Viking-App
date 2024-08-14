abe72f14f231a619fe63d66539a6442182070dfb
D3XTRO12 <mirazopablo@gmail.com>
2024-08-14 15:03:31 -0300

    #7 WorkOrder modificada para aceptar logs de lombok

--- VikingApp/src/main/java/VikingoLab/VikingApp/app/Resources/WorkOrderResource.java
+++ VikingApp/src/main/java/VikingoLab/VikingApp/app/Resources/WorkOrderResource.java
@@ -10,8 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 import VikingoLab.VikingApp.app.Models.WorkOrder;
 import VikingoLab.VikingApp.app.Services.WorkOrderService;
 import VikingoLab.VikingApp.app.Services.WorkOrderService.CustomException;
+import lombok.extern.log4j.Log4j2;
 
 @RestController
+@Log4j2
 @RequestMapping("/work-order")
 public class WorkOrderResource {
 
@@ -25,10 +27,15 @@ public class WorkOrderResource {
     @PostMapping("/save")
     public ResponseEntity<?> createWorkOrder(@RequestBody WorkOrder workOrder) {
         try {
+            log.info("Entrando al método createWorkOrder en WorkOrderResource...");
+            log.info("WorkOrder recibido: {}", workOrder);
+
             WorkOrder createdWorkOrder = workOrderService.createWorkOrder(workOrder);
+            log.info("WorkOrder creado: {}", createdWorkOrder);
+
             return ResponseEntity.ok(createdWorkOrder);
         } catch (CustomException e) {
-            // Manejar la excepción, devolver un mensaje de error al cliente
+            log.error("Error al crear el WorkOrder: {}", e.getMessage());
             return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
