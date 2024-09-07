406e062d5044dc5cbc1f44ae3cc2c5f88a0620e2
Pablo, sometimes <mirazopablo@gmail.com>
2024-09-06 22:44:45 -0300

    #10 Main Class

---
+++ src/main/java/com/ElVikingoStore/Viking_App/VikingAppApplication.java
@@ -0,0 +1,13 @@
+package com.ElVikingoStore.Viking_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VikingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VikingAppApplication.class, args);
+	}
+
+}
