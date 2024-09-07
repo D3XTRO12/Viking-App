4a0bf07181b0dc8bac05e88adce0f5ae4b2ce528
Pablo, sometimes <mirazopablo@gmail.com>
2024-09-06 22:42:27 -0300

    #7 Home Resource

---
+++ src/main/java/com/ElVikingoStore/Viking_App/Resources/HomeResource.java
@@ -0,0 +1,16 @@
+package com.ElVikingoStore.Viking_App.Resources;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/")
+public class HomeResource {
+
+    @GetMapping("/")
+    public String greeting() {
+        return "If you see this message, Welcome to my api -- Status: Running";
+    }
+}
