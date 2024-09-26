fd4bedf6f4e9cae41c1c56d5f60e362af4d1fea6
Pablo, sometimes <mirazopablo@gmail.com>
2024-09-26 13:15:30 -0300

    #7 Workorder resource funcionando

--- src/main/java/com/ElVikingoStore/Viking_App/Resources/WorkOrderResource.java
+++ src/main/java/com/ElVikingoStore/Viking_App/Resources/WorkOrderResource.java
@@ -1,7 +1,12 @@
 package com.ElVikingoStore.Viking_App.Resources;
 
 import com.ElVikingoStore.Viking_App.DTOs.WorkOrderDto;
+import com.ElVikingoStore.Viking_App.Services.UserService;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +14,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.ElVikingoStore.Viking_App.Models.WorkOrder;
 import com.ElVikingoStore.Viking_App.Services.WorkOrderService;
-import com.ElVikingoStore.Viking_App.Services.WorkOrderService.CustomException;
 
 import lombok.extern.log4j.Log4j2;
 
@@ -19,15 +23,32 @@ import lombok.extern.log4j.Log4j2;
 public class WorkOrderResource {
 
     private final WorkOrderService workOrderService;
+    private final UserService userService;
 
-    //@Autowired
-    public WorkOrderResource(WorkOrderService workOrderService) {
+    @Autowired
+    public WorkOrderResource(WorkOrderService workOrderService, UserService userService) {
         this.workOrderService = workOrderService;
+        this.userService = userService;
     }
 
     @PostMapping("/save")
-    public ResponseEntity<WorkOrder> createWorkOrder(@RequestBody WorkOrderDto workOrderDto) {
-        WorkOrder createdWorkOrder = workOrderService.saveWorkOrder(workOrderDto);
-        return ResponseEntity.ok(createdWorkOrder);
+    public ResponseEntity<?> createWorkOrder(@RequestBody WorkOrderDto workOrderDto, Authentication authentication) {
+        // Obtener el usuario autenticado
+        String email = authentication.getName();
+
+        // Verificar si el usuario tiene el rol de staff (rol_id = 1)
+        if (!userService.hasRoleId(email, 1L)) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Access denied: User is not staff");
+        }
+
+        try {
+            WorkOrder createdWorkOrder = workOrderService.saveWorkOrder(workOrderDto);
+            return ResponseEntity.ok(createdWorkOrder);
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        } catch (Exception e) {
+            log.error("Error creating work order", e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error creating work order");
+        }
     }
 }
